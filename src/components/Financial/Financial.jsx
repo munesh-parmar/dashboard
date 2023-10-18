6bd5e9e07425cffacbd6ee5df1d854a7bb6e3cfe
Munesh Kumar <86865319+munesh-parmar@users.noreply.github.com>
2023-10-18 17:46:35 +0500

    Update Financial.jsx

--- src/components/Financial/Financial.jsx
+++ src/components/Financial/Financial.jsx
@@ -10,8 +10,7 @@ const Financial = () => {
             </button>
         </div>
         <div className="grid-c8-content">
-            <p className="text text-silver-v1">Ipsum dolor sit amet consectetur, adipisicing elit.
-                Iste, vitae.....</p>
+            <p className="text text-silver-v1"> Start by tracking your income and expenses. Creating a budget will give you a clear picture of where your money is going and help you identify areas where you can save.</p>
         </div>
     </div>
   )
